97be0178a6a13a3465075555a6203976d1bdbcf3
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-07 17:26:50 +0200

    Implementation of everything except floating points

--- src/ga4gh_json_canonical/__init__.py
+++ src/ga4gh_json_canonical/__init__.py
@@ -1,7 +1,36 @@
 __version__ = '0.1.0'
 
 import json
+from typing import Any, Callable, Dict, Tuple
 
 
-def canonicalize(data):
-    return json.dumps(data)
+def to_utf16_tuple(any_str: str) -> Tuple[int]:
+    utf_16_bytes = any_str.encode('utf-16-be')
+    return tuple(
+        int.from_bytes(utf_16_bytes[i:i + 2], 'big') for i in range(0, len(utf_16_bytes), 2))
+
+
+def key_to_utf16_tuple(keyval: tuple) -> Tuple[int]:
+    key, val = keyval
+    return to_utf16_tuple(key)
+
+
+def sort_all_dicts(data: Any):
+    if type(data) is dict:
+        return dict(
+            sorted(((key, sort_all_dicts(val)) for key, val in data.items()),
+                   key=key_to_utf16_tuple))
+    if type(data) in (list, tuple):
+        return tuple(sort_all_dicts(el) for el in data)
+    return data
+
+
+def canonicalize(data: Any) -> bytes:
+    data = sort_all_dicts(data)
+    output = json.dumps(
+        data,
+        separators=(',', ':'),
+        ensure_ascii=False,
+        allow_nan=False,
+    )
+    return output.encode('utf8')
--- tests/test_ga4gh_json_canonical.py
+++ tests/test_ga4gh_json_canonical.py
@@ -1,10 +1,115 @@
-from ga4gh_json_canonical import __version__, canonicalize
+from ga4gh_json_canonical import __version__, canonicalize, sort_all_dicts, to_utf16_tuple
+import pytest
+
 
 def test_version():
     assert __version__ == '0.1.0'
 
 
+def test_to_utf16_tuple():
+    assert to_utf16_tuple('€') == (8364,)
+    assert to_utf16_tuple('\r') == (13,)
+    assert to_utf16_tuple('דּ') == (64307,)
+    assert to_utf16_tuple('1') == (49,)
+    assert to_utf16_tuple('😀') == (55357, 56832)
+    assert to_utf16_tuple('\x80') == (128,)
+    assert to_utf16_tuple('ö') == (246,)
+
+
+def test_sort_all_dicts():
+
+    assert sort_all_dicts({'b': 2, 'a': 1}) == {'a': 1, 'b': 2}
+    assert sort_all_dicts({'b': {'d': 3, 'c': 2}, 'a': 1}) == {'b': {'c': 2, 'd': 3}, 'a': 1}
+    assert sort_all_dicts({
+        'a': [{
+            'd': 3, 'c': 2
+        }, 4], 'b': 2
+    }) == {
+        'a': ({
+            'c': 2, 'd': 3
+        }, 4), 'b': 2
+    }
+
+
+def test_whitespace():
+    assert canonicalize({'a': [2, 3, 4, {'b': 2}, 3]}) == b'{"a":[2,3,4,{"b":2},3]}'
+
+
 def test_literals():
-    assert canonicalize(None) == 'null'
-    assert canonicalize(True) == 'true'
-    assert canonicalize(False) == 'false'
+    assert canonicalize(None) == b'null'
+    assert canonicalize(True) == b'true'
+    assert canonicalize(False) == b'false'
+
+
+def test_strings():
+    for c in range(8):
+        assert canonicalize(chr(c)) == b'"\\u00%(c)02x"' % {b'c': c}
+
+    assert canonicalize(chr(8)) == b'"\\b"'
+
+    assert canonicalize(chr(9)) == b'"\\t"'
+
+    assert canonicalize(chr(10)) == b'"\\n"'
+
+    assert canonicalize(chr(11)) == b'"\\u000b"'
+
+    assert canonicalize(chr(12)) == b'"\\f"'
+
+    assert canonicalize(chr(13)) == b'"\\r"'
+
+    for c in range(14, 32):
+        assert canonicalize(chr(c)) == b'"\\u00%(c)02x"' % {b'c': c}
+
+    assert canonicalize(chr(32)) == b'" "'
+
+    for c in range(35, 92):
+        assert canonicalize(chr(c)) == bytes(f'"{chr(c)}"', 'utf8')
+
+    assert canonicalize(chr(92)) == b'"\\\\"'
+
+    for c in range(93, 55296):
+        assert canonicalize(chr(c)) == bytes(f'"{chr(c)}"', 'utf8')
+
+    for c in range(55296, 57344):
+        with pytest.raises(UnicodeEncodeError):
+            canonicalize(chr(c))
+
+    for c in range(57344, 65536):
+        assert canonicalize(chr(c)) == bytes(f'"{chr(c)}"', 'utf8')
+
+    assert canonicalize('€') == bytes(f'"€"', 'utf8')
+    assert canonicalize('דּ') == bytes(f'"דּ"', 'utf8')
+    assert canonicalize('😀') == bytes(f'"😀"', 'utf8')
+    assert canonicalize('\x80') == bytes(f'"\x80"', 'utf8')
+    assert canonicalize('ö') == bytes(f'"ö"', 'utf8')
+
+
+def test_numbers():
+    with pytest.raises(ValueError):
+        assert canonicalize(float('nan'))
+    with pytest.raises(ValueError):
+        assert canonicalize(float('inf'))
+
+
+def test_sorting():
+    input = {
+        '€': 'Euro Sign',
+        '\r': 'Carriage Return',
+        'דּ': 'Hebrew Letter Dalet With Dagesh',
+        '1': 'One',
+        '😀': "Emoji: Grinning Face",
+        '\x80': "Control",
+        'ö': "Latin Small Letter O With Diaeresis"
+    }
+
+    output = bytes(
+        '{"\\r":"Carriage Return",'
+        '"1":"One",'
+        '"\u0080":"Control",'
+        '"ö":"Latin Small Letter O With Diaeresis",'
+        '"€":"Euro Sign",'
+        '"😀":"Emoji: Grinning Face",'
+        '"דּ":"Hebrew Letter Dalet With Dagesh"}',
+        'utf8')
+
+    assert canonicalize(input) == output
