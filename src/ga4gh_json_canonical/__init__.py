1f2df7855f8551c533edfa84af112cfa788e54ff
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-07 09:47:29 +0200

    First test

--- src/ga4gh_json_canonical/__init__.py
+++ src/ga4gh_json_canonical/__init__.py
@@ -1,1 +1,7 @@
 __version__ = '0.1.0'
+
+import json
+
+
+def canonicalize(data):
+    return json.dumps(data)
--- tests/test_ga4gh_json_canonical.py
+++ tests/test_ga4gh_json_canonical.py
@@ -1,5 +1,10 @@
-from ga4gh_json_canonical import __version__
-
+from ga4gh_json_canonical import __version__, canonicalize
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_literals():
+    assert canonicalize(None) == 'null'
+    assert canonicalize(True) == 'true'
+    assert canonicalize(False) == 'false'
